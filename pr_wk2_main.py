8e8b3851f16308c558731c587782bf541cf98f51
Enantiodromis <georgerebradley@gmail.com>
2021-02-01 14:35:16 +0000

    Adding, explanatory comments to both files.

--- pr_wk2_main.py
+++ pr_wk2_main.py
@@ -1,21 +1,40 @@
 from sklearn import datasets
 from widrow_hoff_learning_algorithm import widrow_hoff_algorithm
 
-# TRAINING DATA:
-# Some of the exercises the Iris dataset. This dataset contains 150 samples from 3 classes. 
-# Each sample is a four dimensional feature vector. This data can be obtained as follows.
+# QUESTION 1: Apply 2 epochs of the Sequential Widrow-Hoff Learning Algorithm to the training data.
+#
+# Use initial parameters values:
+# aT=(w0,wT)=(1.0,0.0,0.0)
+# Margin vector bT=(1.0,0.5,0.5,1.5,0.5,1.0), 
+# A learning rate of 0.1. 
 
-# The structure "iris" contains a field iris.data that is an array, each row of which
-# is a feature vector for one sample, and a field iris.target that is a vector defining
-# the class labels associated with the corresponding rows of iris.data.
-iris = datasets.load_iris()
-
-# INITIALISING VARIABLES AND CALLING WIDROW-HOFF LEARNING ALGORITHM FUNCTION
+# Initialising inputs and calling the widrow_hoff_learning_algotithm function.
 a = [1.0,0.0,0.0]
 b = [1.0,0.5,0.5,1.5,0.5,1.0]
 n = 0.1
 epoch = 2
 X = [[1],[1],[1],[-1],[-1],[-1]]
 Y = [[0.0, 2.0], [1.0, 2.0], [2.0, 1.0], [-3.0, 1.0], [-2.0, -1.0], [-3.0, -2.0]]
-
 widrow_hoff_algorithm(a,b,n,epoch,Y,X)
+
+# TRAINING DATA:
+# The Iris dataset. This dataset contains 150 samples from 3 classes. 
+# Each sample is a four dimensional feature vector.
+# The structure "iris" contains a field iris.data that is an array, each row of which
+# is a feature vector for one sample, and a field iris.target that is a vector defining
+# the class labels associated with the corresponding rows of iris.data.
+
+# Loading the iris dataset, a import also included
+iris = datasets.load_iris()
+
+# QUESTION 2: Use the Widrow-Hoff Learning Algorithm to find the parameters of a 
+# linear discriminant function that should output g(x)>0 for class 0 and g(x)≤0 for the other two classes of the iris dataset.
+# Apply 2 epochs of the Sequential Widrow-Hoff Learning Algorithm. 
+#
+# Use initial parameter values:
+# -> aT=(w0,wT)=(0.5,0.5,−2.5,1.5,−0.5)
+# -> Margin vector b in which all values are equal to 1 
+# -> A learning rate of 0.01.
+#
+# Calculate the percentage of samples for which the linear discriminant function produces the desired output 
+# (i.e. g(x)>0 for the 1st 50 samples, and g(x)≤0 for the remaining 100 samples) using the initial parameter values, and those calculated after learning for 2 epochs.
--- widrow_hoff_learning_algorithm.py
+++ widrow_hoff_learning_algorithm.py
@@ -2,43 +2,39 @@ import numpy as np
 from prettytable import PrettyTable
 from sklearn import preprocessing
 
-# DEFINING FUNCTION FOR WIDROW-HOFF LEARNING ALGORITHM:
-# The Widrow-Hoff Learning Algorithm is applied to the following data in order to learn the parameters 
-# of a linear discriminant function:
-# 
-# Feature vector, xT: [[0, 2], [1, 2], [2, 1], [-3, 1], [-2, 1], [-3, 2]]
-# Class: [1, 1, 1, -1, -1, -1]
-#
-# 2 epochs of the Sequential Widrow-Hoff Learning Algorithm were applied to the training data
-# The initial parameters values of aT = (w0, wT) = (1.0,0.0,0.0), margin vector bT = (1.0,0.5,0.5,1.5,0.5,1.0) 
-# and a learning rate of 0.1 were used.
+# SEQUENTIAL WIDROW HOFF LEARNING ALGORITHM BASED MINIMISATION
+# - Set values of the hyper-parameters(η and b)
+# - Initialise a to arbritrary solution
+# - For each sample, yk in the dataset in turn
+#   -> update solution: a←a+η(bk−at yk) yk
 
 def widrow_hoff_algorithm(a, b, n, epoch, Y, X):
-    result = []
-    for i in range(len(Y)): Y[i].insert(0, 1.0)
+    result = [] # Defining a list of results, used later for displaying in a table.
+    for i in range(len(Y)): Y[i].insert(0, 1.0) # Adding "1.0" into the first position of every element of Y used for normalisation
     
-    for iter1 in range(epoch):
-        for iter2 in range(len(Y)):
-            # NORMALISING DATA
+    for iter1 in range(epoch): # Iterating over epoch
+        for iter2 in range(len(Y)): # Iterating through ever element within Y
+            # STEP 1: NORMALISING DATA
             y = np.multiply(Y[iter2], X[iter2])
          
-            # CALCULATING: "aTyk"
+            # STEP 2: CALCULATING: "aTyk"
             ay = np.dot(a, y)
     
-            # CALCULATING UPDATE: "aTnew=aT+η(bk−aTyk)yTk"
+            # STEP 3: CALCULATING UPDATE: "aTnew=aT+η(bk−aTyk)yTk"
             update = np.zeros(len(y))
             for iter3 in range(len(y)): 
-                update[iter3] = n * (b[iter2] - ay) * y[iter3]
+                update[iter3] = n * (b[iter2] - ay) * y[iter3] # η(bk−at yk) yk
             
-            # ADDING UPDATED PARTS TO A
-            a = np.add(a, update)
+            # STEP 4: ADDING UPDATED PARTS TO A
+            a = np.add(a, update) # a←a+η(bk−at yk) yk
 
             # APPENDING RESULTS TO ARRAY, SPECIFICALLY FOCUSING ON 3 COLUMNS, ITERATIONS, AY AND A_NEW
             result.append((str(iter2 + 1 + (len(Y) * iter1)),np.round(ay, 2), np.round(a, 2)))
-            
+    
+    # Defining a table to display results
     results_table = PrettyTable(('iteration', 'ay', 'a_new'))
+    
     for row in result: results_table.add_row(row)
-
     results_table.align['Iteration'] = 'c'
     results_table.align['ay'] = 'l'
     results_table.align['a_new'] = 'l'
